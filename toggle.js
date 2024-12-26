d3e55866b75353962883c5356f4ca8423c5d4c4c
PranavMishra07 <pranav072003@gmail.com>
2024-12-26 22:37:07 +0530

    a

---
+++ toggle.js
@@ -0,0 +1,19 @@
+function toggle() {
+    // let head = document.querySelector("#head")
+    // head.classList.toggle('head')
+    // head.innerHTML = "Pranav"
+    // let h1 = document.querySelector("#butt")
+    // h1.innerHTML = "change"
+    let but = document.querySelector("#but")
+
+    but.classList.toggle('toggle')
+    // let butt = document.querySelector("#butt")
+    // butt.classList.toggle('butt')
+    // butt.innerHTML = "jhghdf"
+    but.innerHTML = "change"
+    let head = document.querySelector("#head")
+    head.innerHTML = "Pramav"
+
+
+
+}
