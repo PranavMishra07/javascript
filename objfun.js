eed7eb169f23198210f7cb3c76ed948baef0896e
PranavMishra07 <pranav072003@gmail.com>
2025-01-02 19:49:42 +0530

    S

--- objfun.js
+++ objfun.js
@@ -1,3 +1,4 @@
+// In JavaScript, an object is a collection of key-value pairs (often referred to as properties), where the key is a string (also called a property name) and the value can be any data type, including other objects, arrays, or functions.
 function obj() {
     let obj = {
         name: document.querySelector("#name").value,
@@ -11,3 +12,42 @@ function obj() {
     return false
 
 }
+let obj1 = {
+    name: "parent",
+    contact: 12345   //
+}
+obj1.city = "bhopal"  // add key
+obj1.contact = 2223   //edit number
+console.log(obj1);
+
+let emp = {
+    emp_name: "Aaditya",
+    emp_id: 201,
+    emp_age: 21,
+    emp_designation: "Developer",
+    emp_account: {
+        emp_salary: 15000,
+        emp_bankname: "SBI",
+        emp_acbalance: "Rs 4/- Only"
+    }
+
+}
+console.log(emp.emp_account);
+
+console.log(emp.emp_account.emp_acbalance);
+
+let obj2 = {
+    name: "Anuj",
+    demo: function () {   //function
+        alert("inside Object")
+    },
+    friend: ["harsh", "aman", "prabal", "himanshu"],  //araay
+    fun: function () {
+        console.log(this.name);
+
+    }
+}
+obj2.demo()
+obj2.fun()
+console.log(obj2.friend[0])
+
--- spread.js
+++ spread.js
@@ -5,17 +5,13 @@ let ar1 = [1, 2, 3]
 let ar2 = [4, 5, 6]
 let ar3 = [...ar1, ...ar2]
 console.log(ar3);
-//spread operator for merging  array 
-
 // The slice() function in JavaScript is used to extract a shallow copy of a portion of an array or a string without modifying the original array or string. It returns a new array or string that contains elements or characters from the original array/string, starting from the specified start index and optionally ending at the end index.
 let arr = [1, 2, 3, 4, 5, 6, 7]
 console.log(arr);
 let del = arr.slice(1, 5)
 console.log(del)
 console.log(arr);
-
 //The splice() method in JavaScript is used to modify an array by adding, removing, or replacing elements at a specific index. Unlike the slice() method, which creates a shallow copy of a portion of the array, splice() directly changes the original array.
-
 arr.splice(1, 4)
 console.log(arr)
 arr.splice(1, 0, "cose", "adas")
