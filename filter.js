3970e4379f0ef2501a8ba8410851adf59fe06132
PranavMishra07 <pranav072003@gmail.com>
2025-01-06 19:51:14 +0530

    map

---
+++ filter.js
@@ -0,0 +1,6 @@
+let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(neu);
+
+
+let nar = ar.filter((f) => { return f % 2 == 0 })
+console.log(nar);
--- map.js
+++ map.js
@@ -5,5 +5,36 @@ let ar = [1, 2, 3, 4, 5, 6, 7]
 let neu = ar.map((t) => { return t > 2 })
 
 
-//only on array
+//only on array   to fetch elements of array
 console.log(neu);
+
+
+let nar = ar.filter((f) => { return f % 2 == 0 })
+console.log(nar);
+
+
+let arr1 = [
+    {
+        id: 1,
+        Name: "Atul",
+        Age: 21
+    },
+    {
+        id: 2,
+        Name: "viplob",
+        Age: 30,
+    },
+    {
+        id: 3,
+        Name: "avinash",
+        Age: 12
+    }
+
+]
+let st = arr1.map((s) => { return s.id })
+console.log(st);
+
+let st_filter = arr1.filter((f) => { return f.Age > 20 })
+console.log(st_filter);
+
+
