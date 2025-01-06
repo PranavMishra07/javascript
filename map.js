234873b18a48d75b8584e327db834cf269d45959
PranavMishra07 <pranav072003@gmail.com>
2025-01-06 19:27:04 +0530

    a

---
+++ map.js
@@ -0,0 +1,9 @@
+let ar = [1, 2, 3, 4, 5, 6, 7]
+// let neu = ar.map((t) => { return t })
+// let neu = ar.map((t) => { return t + 2 })
+// let neu = ar.map((t) => { return t * 2 })
+let neu = ar.map((t) => { return t > 2 })
+
+
+//only on array
+console.log(neu);
