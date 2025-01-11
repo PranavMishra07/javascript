9ca9aabdbce88e4242a0fc03d3de6897161a6468
PranavMishra07 <pranav072003@gmail.com>
2025-01-11 19:44:48 +0530

    dd

--- timer.js
+++ timer.js
@@ -2,12 +2,16 @@ let st
 function start() {
     let head = document.querySelector("#head")
     let num = 0;
-    st = setInterval(() => { head.innerHTML = num++ }, 1000)
+    st = setInterval(() => { head.innerHTML = num++ }, 1000)   // 1000 1second     infinite time call
 }
+let r = setInterval(() => {
+    document.write("ello")
+})
+clearInterval(r)
 function stop() {
     clearInterval(st)
 }
-setTimeout(() => alert("settimeout Call"), 2000)
+setTimeout(() => alert("settimeout Call"), 2000)    //only once call
 
 let btn = document.querySelector(".btn")
 //           first event name, function
