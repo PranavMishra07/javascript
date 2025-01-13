4c2b84ef6361b5fa12c4e4181e0315f42dd2d085
PranavMishra07 <pranav072003@gmail.com>
2025-01-13 19:45:20 +0530

    sa

---
+++ factor.js
@@ -0,0 +1,11 @@
+// function submit() {
+//     let i
+//     let input = [,] = document.querySelector("#fact").value
+//     for (i = 0; i <= input; i++) {
+//         if (input % i == 0) {
+//             console.log(i);
+
+
+//         }
+//     }
+// }
