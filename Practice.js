607ab356596e28ff080c0059b25c43cae230f602
PranavMishra07 <pranav072003@gmail.com>
2025-03-13 15:00:05 +0530

    sd

---
+++ Practice.js
@@ -0,0 +1,12 @@
+let x = null;
+console.log(x);//null
+
+let y
+console.log(y);//undefined
+
+const obj = {}
+console.log(obj.age);//undefined
+
+function noReturn() { }
+console.log(noReturn()); //undefined
+
