b36769417871cb2e5ffcb2a0cb7b2e8dac2c68b0
PranavMishra07 <pranav072003@gmail.com>
2025-07-04 08:34:16 +0530

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -38,7 +38,7 @@ debugger
 console.log(x)
 console.log(x)
 
-console.log(null === "0");
+console.log(null == "0");
 console.log(5 == "5"); // true (string "5" is converted to number 5)
 console.log(0 == false); // true (false is converted to 0)
 console.log(null == undefined); // true (special case)
